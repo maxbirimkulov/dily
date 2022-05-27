29fdbe7fc333a0ebe548a6709aa8a68729a51b2f
maxbirimkulov <maxbirimkulov@mail.ru>
2022-05-27 20:51:13 +0600

    first add

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import {useEffect} from "react";
+import {db, storage} from "./firebase/firebase";
+import {collection, getDocs, addDoc, doc, updateDoc, deleteDoc} from "firebase/firestore";
+import {getAllProducts} from "./redux/reducers/products";
+import {useDispatch, useSelector} from "react-redux";
+import {Routes, Route} from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Shops from "./pages/Shops/Shops";
+import Charity from "./pages/Charity/Charity";
+import Layout from "./Layout/Layout";
+import Auth from "./pages/Auth/Auth";
 
 function App() {
+
+    const dispatch = useDispatch()
+
+    const products = useSelector((s) => s.products.products)
+
+
+    useEffect(() => {
+        getDocs(collection(db, 'products'))
+            .then((res) => dispatch(getAllProducts({arr: res.docs.map(el => ({...el.data(), id:el.id}) )})))
+    },[])
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Routes>
+
+            <Route path='/' element={<Layout/>}>
+                <Route path='' element={<Home/>}/>
+                <Route path='shops' element={<Shops/>}/>
+                <Route path='charity' element={<Charity/>}/>
+            </Route>
+
+
+            <Route path='/auth' element={<Auth/>}/>
+        </Routes>
     </div>
   );
 }
